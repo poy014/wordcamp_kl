065dc03cdf205cc5167e4396697f59003cf9c150
ian labao <ian.labao@viewqwest.com>
2019-10-31 23:09:33 +0800

    STEP 2: Add CPT function on INIT hook of WP

--- plugins/my-custom-post-type/includes/class-my-custom-post-type.php
+++ plugins/my-custom-post-type/includes/class-my-custom-post-type.php
@@ -157,6 +157,9 @@ class My_Custom_Post_Type {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		// add CPT  on INIT of WP
+		$this->loader->add_action( 'init', $plugin_admin, 'my_cpt' );
+
 	}
 
 	/**
